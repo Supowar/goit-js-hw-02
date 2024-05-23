35e5d35f41b4355584e747b14e00a89e3c3999c3
Supowar <152823865+Supowar@users.noreply.github.com>
2024-05-23 16:05:07 +0300

    addjs2

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+function checkForSpam(message) {
+  // Приводимо рядок до нижнього регістру для перевірки
+  const lowerCaseMessage = message.toLowerCase();
+
+  // Перевіряємо наявність заборонених слів
+  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Перевірка роботи функції
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
